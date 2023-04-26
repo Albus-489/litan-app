f0591748c022d75e9ac80b1d54407ae70eee47e8
Albus-489 <smartoctopusz@gmail.com>
2023-04-26 16:18:29 +0300

    axios on b-r

--- src/components/books-row.component.tsx
+++ src/components/books-row.component.tsx
@@ -4,13 +4,16 @@ import { IBook } from "./models/interfaces/IBook";
 import { Book } from "./models/Books";
 import Marginalia from "./marginalia.component";
 import bookanData from "../data/bookan.json";
+import axios from "axios";
+import { fetchLitans } from "./funcs/axios/fetchLitans";
 const boockCover = require("./images/bookCover.png");
 
 type BooksRowProps = {
   books: IBook[];
+  setBooks: React.Dispatch<React.SetStateAction<IBook[]>>;
 };
 
-const BooksRow: React.FC<BooksRowProps> = ({ books }) => {
+const BooksRow: React.FC<BooksRowProps> = ({ books, setBooks }) => {
   const [currentPage, setCurrentPage] = useState("Home");
   const [isContentVisible, setContentVisibility] = useState(true);
   const [curentAnalysis, setCurentAnalysis] = useState<IBook>();
@@ -21,11 +24,25 @@ const BooksRow: React.FC<BooksRowProps> = ({ books }) => {
     setContentVisibility(false);
   }
 
+  async function handleDelete(id: string) {
+    try {
+      const response = await axios.delete(`http://localhost:8080/litans/${id}`);
+      fetchLitans(books, setBooks);
+      console.log(response.data, `ID:${id}`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // async function handleAddNewBA(){
+  //   try
+  // }
+
   return (
     <>
       {isContentVisible ? (
         <div className="books-row container">
-          {bookanData.map((book, index) => (
+          {books.map((book, index) => (
             <div className="card bg-dark" key={index}>
               <img
                 id="book-image"
@@ -34,19 +51,28 @@ const BooksRow: React.FC<BooksRowProps> = ({ books }) => {
                 alt="..."
               />
               <div className="card-body">
-                <h5 className="card-title">{book.name}</h5>
-                <button
-                  onClick={() => handleOpen(book)}
-                  className="btn btn-primary"
-                >
-                  Open
-                </button>
+                <h5 className="card-title text-center">{book.name}</h5>
+                <div className="cardBtns d-flex justify-content-between">
+                  <button
+                    onClick={() => handleOpen(book)}
+                    className="btn btn-primary"
+                  >
+                    Open
+                  </button>
+                  <button
+                    onClick={() => handleDelete(book._id!)}
+                    className="btn btn-danger ml-2"
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
             </div>
           ))}
           <div className="addNewBAbox">
             <span
-              className="addNewBAtext"
+              //
+              className="addNewBAtext p-2"
               data-bs-toggle="modal"
               data-bs-target="#exampleModal"
             >
