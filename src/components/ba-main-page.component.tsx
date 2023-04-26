39d5401879d40708c60c98c0acc23668a1be34a5
Albus-489 <smartoctopusz@gmail.com>
2023-04-26 16:20:34 +0300

    fetch feature

--- src/components/ba-main-page.component.tsx
+++ src/components/ba-main-page.component.tsx
@@ -6,16 +6,17 @@ import "./styles/books-row.css";
 import Button from "react-bootstrap/Button";
 import BooksRow from "./books-row.component";
 import ModalComponent from "./modal.component";
+import UploadFile from "./uploadlitan.component";
+import { getAllLitans } from "./funcs/axios/getAllLitans";
+import { fetchLitans } from "./funcs/axios/fetchLitans";
 const boockCover = require("./images/bookCover.png");
 
 const BookAnalysisMainPage = () => {
   const [books, setBooks] = useState<IBook[]>([]);
 
-  const logs = () => {
-    const b = new Book("Aboba");
-    const notes = b.getFullNotes();
-    console.log(notes);
-  };
+  useEffect(() => {
+    fetchLitans(books, setBooks);
+  }, []);
 
   // const testGetBook = async () => {
   //   const a = await getBooks();
@@ -24,7 +25,7 @@ const BookAnalysisMainPage = () => {
   return (
     <div className="wrapper">
       {/* BOOKS ROW */}
-      <BooksRow books={books} />
+      <BooksRow books={books} setBooks={setBooks} />
 
       {/* MODAL */}
       <ModalComponent books={books} setBooks={setBooks} />
