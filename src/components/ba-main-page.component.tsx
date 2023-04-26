137924f1df490ffd3bef7f938bf36cec800501d9
Albus-489 <bavrzar@gmail.com>
2023-04-26 23:31:51 +0300

    Main cleaning

--- src/components/ba-main-page.component.tsx
+++ src/components/ba-main-page.component.tsx
@@ -1,15 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { createBook } from "./funcs/createBook";
-import { Book } from "./models/Books";
 import { IBook } from "./models/interfaces/IBook";
 import "./styles/books-row.css";
-import Button from "react-bootstrap/Button";
 import BooksRow from "./books-row.component";
 import ModalComponent from "./modal.component";
-import UploadFile from "./uploadlitan.component";
-import { getAllLitans } from "./funcs/axios/getAllLitans";
 import { fetchLitans } from "./funcs/axios/fetchLitans";
-const boockCover = require("./images/bookCover.png");
 
 const BookAnalysisMainPage = () => {
   const [books, setBooks] = useState<IBook[]>([]);
@@ -18,17 +12,14 @@ const BookAnalysisMainPage = () => {
     fetchLitans(books, setBooks);
   }, []);
 
-  // const testGetBook = async () => {
-  //   const a = await getBooks();
-  //   console.log(a);
-  // };
   return (
     <div className="wrapper">
       {/* BOOKS ROW */}
       <BooksRow books={books} setBooks={setBooks} />
 
-      {/* MODAL */}
+      {/* MODALS */}
       <ModalComponent books={books} setBooks={setBooks} />
+      {/* <ModalComponentUpdate books={books} setBooks={setBooks} /> */}
     </div>
   );
 };
