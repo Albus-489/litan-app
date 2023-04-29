507d2e8f7416929f30fb2e460945bf8d4dfd05fe
Albus-489 <bavrzar@gmail.com>
2023-04-29 20:33:34 +0300

    Handle open changed to sync when open

--- src/components/funcs/handlers/litan.handlers.ts
+++ src/components/funcs/handlers/litan.handlers.ts
@@ -1,11 +1,13 @@
 import { IBook } from "../../models/interfaces/IBook";
+import getLitanById from "../axios/getLitanById";
 
-export function handleOpen(
+export async function handleOpen(
   analysis: IBook,
   setCurentAnalysis: React.Dispatch<React.SetStateAction<IBook | undefined>>,
   setContentVisibility: React.Dispatch<React.SetStateAction<boolean>>
 ) {
-  setCurentAnalysis(analysis);
+  const litan = await getLitanById(analysis._id!);
+  setCurentAnalysis(litan);
   setContentVisibility(false);
 }
 
